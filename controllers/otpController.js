60e4bfc1db622614bcfeb03c06e7e22e7b131a74
Rajender <rajender.cheerneni@heterohealthcare.com>
2024-01-31 10:47:49 +0530

    registration fileds are changed

---
+++ controllers/otpController.js
@@ -0,0 +1,51 @@
+const db = require('../dbconfig');
+
+const generateOTP = () => {
+  // Generate a random 4-digit number
+  const otp = Math.floor(1000 + Math.random() * 9000);
+
+  // Ensure the generated number is exactly 4 digits
+  return String(otp).padStart(4, '0');
+};
+
+exports.sendOtp = catchasync(async (req, res, next) => {
+  const { empId } = req.body;
+  const getEmployee = `SELECT emp_id ,mobile_num FROM employees_tbl WHERE emp_id = '${empId}'`;
+
+  db.query(getEmployee, (err, result) => {
+    if (err) {
+      return next(new AppError(err.sqlMessage, 400));
+    }
+    if (result.length > 0) {
+      const { emp_id, mobile_num } = result[0];
+      const Otp = generateOTP();
+      console.log(Otp);
+      // Store the OTP and its expiration time (e.g., 5 minutes)
+      const otpData = { Otp, expiresAt: Date.now() + 5 * 60 * 1000 };
+
+      otpStorage[emp_id] = otpData;
+
+      res.status(200).json({ id: emp_id, Otp });
+    } else {
+      res.status(404).send({ message: 'Invalid Employee Id' });
+    }
+  });
+});
+
+exports.verifyOTP = catchasync(async (req, res, next) => {
+  const { empId, otp } = req.body;
+  // Check if OTP exists and is not expired
+  const otpData = otpStorage[empId];
+
+  if (!otpData || otpData.expiresAt < Date.now()) {
+    return res.status(400).json({ message: 'OTP expired or does not exist' });
+  }
+  // Verify the provided OTP
+  if (otp === otpData.Otp) {
+    const payload = { empId };
+    const jwtToken = jwt.sign(payload, process.env.JWT_SECRET);
+    res.status(200).json({ jwtToken, message: 'OTP verification successful' });
+  } else {
+    res.status(400).json({ message: 'Invalid OTP' });
+  }
+});
