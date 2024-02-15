95ca132144e7d014e626abebe14e122ad676531f
Rajender <rajender.cheerneni@heterohealthcare.com>
2024-02-15 17:29:38 +0530

    product varient and details api done

--- controllers/ProductCtrl/productDetailsCtrl.js
+++ controllers/ProductCtrl/productDetailsCtrl.js
@@ -72,7 +72,7 @@ exports.getCollectionProducts = catchAsync(async (req, res, next) => {
   const { collectionId } = req.body; // Assuming collectionId is a single value
 
   const getProducts = `SELECT id as product_id, display_name, short_name, product_name, vendor_id, 
-                        type, tags, published, image_src,image_position,
+                        type, tags, DATE_FORMAT(published, '%d-%m-%Y') AS published_date, image_src,image_position,
                         image_alt_text,cost_per_item, price_india, status
                        FROM azst_products
                        WHERE product_category ->> '$[*]' LIKE CONCAT('%', ?, '%')  AND azst_products.status = 1`;
@@ -97,52 +97,57 @@ exports.getCollectionProducts = catchAsync(async (req, res, next) => {
 exports.getProductDetalis = catchAsync(async (req, res, next) => {
   const { productId } = req.body;
 
-  const productDetails = `SELECT azst_vendor_details.azst_vendor_name,azst_products.type,azst_products.tags,
-                            DATE_FORMAT(azst_products.published, '%d-%m-%Y') AS published_date,
-                            azst_products.variant_sku,azst_products.variant_grams,azst_products.variant_inventory_tracker,
-                            azst_products.variant_inventory_policy,azst_products.variant_fulfillment_service,
-                            azst_products.variant_requires_shipping,azst_products.variant_taxable,azst_products.cost_per_item,
-                            azst_products.price_india,azst_sku_variant_info.product_id,azst_sku_variant_info.display_name,
-                            azst_sku_variant_info.short_name,azst_sku_variant_info.product_name,azst_sku_variant_info.variant_image,
-                            azst_sku_variant_info.variant_weight_unit,azst_sku_variant_info.variant_HS_code,
-                            azst_sku_variant_info.variant_barcode,azst_sku_variant_info.variant_images,
-                            azst_sku_variant_info.color,azst_sku_variant_info.size,azst_sku_variant_info.actual_price,
-                            azst_sku_variant_info.offer_price,azst_sku_variant_info.offer_percentage,
-                            azst_product_details.sku_id,azst_product_details.product_description,
-                            azst_product_details.product_highlights,azst_product_details.product_ingredients,
-                            azst_product_details.product_benefits,azst_product_details.product_how_to_use,
-                            azst_product_details.product_specifications
-                        FROM azst_products
-                          JOIN azst_sku_variant_info ON azst_products.id = azst_sku_variant_info.product_id
-                          JOIN azst_product_details ON azst_products.id = azst_product_details.product_id
-                          JOIN azst_vendor_details ON azst_products.vendor = azst_vendor_details.azst_vendor_id
-                        WHERE azst_products.id = ? AND azst_products.status = 1`;
+  const getproductDetails = `SELECT 
+            azst_products.display_name,
+            azst_products.short_name,
+            azst_products.product_name,
+            azst_products.type,
+            azst_products.tags,
+            azst_products.product_images,
+            azst_product_details.product_id,
+            azst_product_details.product_description,
+            azst_product_details.product_highlights,
+            azst_product_details.product_ingredients,
+            azst_product_details.product_benefits,
+            azst_product_details.product_how_to_use,
+            azst_product_details.product_specifications,
+            azst_vendor_details.azst_vendor_name
+          FROM azst_products
+            JOIN azst_product_details ON azst_products.id = azst_product_details.product_id
+            JOIN azst_vendor_details ON azst_products.vendor_id = azst_vendor_details.azst_vendor_id
+          WHERE azst_products.id = ? AND azst_products.status = 1`;
 
-  db.query(productDetails, [productId], (err, results) => {
+  //  azst_sku_variant_info.id as varient_id,
+  //           azst_sku_variant_info.color,
+  //           azst_sku_variant_info.size, JOIN azst_sku_variant_info ON azst_products.id = azst_sku_variant_info.product_id
+
+  const getVariants = `SELECT azst_sku_variant_info.id as varient_id, azst_sku_variant_info.color,azst_sku_variant_info.size
+                      FROM  azst_sku_variant_info WHERE product_id = ? AND status = 1`;
+
+  db.query(getproductDetails, [productId], (err, results) => {
     if (err) return next(new AppError(err.sqlMessage, 400));
     if (results.length === 0)
       return res
         .status(200)
         .json({ productDetails: {}, message: 'No product found' });
 
-    const productDetails = results.map((product) => ({
+    const product = results[0];
+
+    const productDetails = {
       ...product,
-      variant_image: `${req.protocol}://${req.get(
-        'host'
-      )}/product/variantimage/${product.variant_image}`,
-
-      variant_barcode: `${req.protocol}://${req.get(
-        'host'
-      )}/variant/barcode/image/${product.variant_barcode}`,
-
-      variant_images: JSON.parse(product.variant_images).map(
+      product_images: JSON.parse(product.product_images).map(
         (variant_image) =>
           `${req.protocol}://${req.get('host')}/product/images/${variant_image}`
       ),
-    }));
+    };
 
-    res
-      .status(200)
-      .json({ productDetails, message: 'Data retrieved successfully.' });
+    db.query(getVariants, [productId], (err, result) => {
+      if (err) return next(new AppError(err.sqlMessage, 400));
+      res.status(200).json({
+        productDetails,
+        variants: result,
+        message: 'Data retrieved successfully.',
+      });
+    });
   });
 });
