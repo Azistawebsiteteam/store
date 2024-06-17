84cac649c2dbbffe8ef26d47b3f8c61d2df5f4bd
Rajender <rajender.cheerneni@heterohealthcare.com>
2024-06-17 16:36:43 +0530

    Subcategory module added and api's done

--- controllers/AdminCtrls/categoryCtrl.js
+++ controllers/AdminCtrls/categoryCtrl.js
@@ -24,8 +24,36 @@ exports.getcategories = catchAsync(async (req, res, next) => {
   res.status(200).json(categories);
 });
 
+exports.getSubcategories = catchAsync(async (req, res, next) => {
+  const { categoryId } = req.body;
+  console.log(categoryId, 'subcategories');
+  const categoryQuery = `SELECT azst_sub_category_id,azst_sub_category_name 
+                         FROM azst_sub_category_tbl
+                         WHERE azst_sub_category_status = 1 AND azst_category_id = ? `;
+  const categories = await db(categoryQuery, [categoryId]);
+  res.status(200).json(categories);
+});
+
+exports.getAdminSubCategories = catchAsync(async (req, res, next) => {
+  const categoryQuery = `SELECT azst_sub_category_id,azst_sub_category_name 
+                         FROM azst_sub_category_tbl`;
+  const categories = await db(categoryQuery);
+  res.status(200).json(categories);
+});
+
 exports.getCategory = catchAsync(async (req, res, next) => {
   res.status(200).json(req.category);
+});
+
+exports.getSubCategory = catchAsync(async (req, res, next) => {
+  const { subCategoryId } = req.body;
+  const getCategory = `SELECT azst_sub_category_id,azst_sub_category_name,azst_category_id
+                        FROM azst_sub_category_tbl
+                        WHERE  azst_sub_category_id = ${subCategoryId} AND azst_sub_category_status = 1`;
+  const category = await db(getCategory);
+  if (category.length === 0)
+    return next(new AppError('No category found', 404));
+  res.status(200).json(category[0]);
 });
 
 exports.addcategory = catchAsync(async (req, res, next) => {
@@ -65,7 +93,7 @@ exports.addSubCategory = catchAsync(async (req, res, next) => {
   const values = [categoryName, categoryId, req.empId];
 
   const result = await db(insertQuery, values);
-  res.status(200).json({ azst_category_id: result.insertId });
+  res.status(200).json({ azst_sub_category_id: result.insertId });
 });
 
 exports.updatecategory = catchAsync(async (req, res, next) => {
@@ -82,11 +110,36 @@ exports.updatecategory = catchAsync(async (req, res, next) => {
   res.status(200).json({ message: 'Updated category ' + categoryName });
 });
 
+exports.updateSubCategory = catchAsync(async (req, res, next) => {
+  const { subCategoryId, categoryName, categoryId } = req.body;
+
+  if (!categoryName)
+    return next(new AppError('Category Name is Required', 400));
+  if (!categoryId) return next(new AppError('CategoryId is Required', 400));
+
+  const updateQuery =
+    'UPDATE azst_sub_category_tbl SET azst_sub_category_name=?,azst_category_id = ?, updated_by=? where azst_sub_category_id =? ';
+  const values = [categoryName, categoryId, req.empId, subCategoryId];
+
+  await db(updateQuery, values);
+  res.status(200).json({ message: 'Updated subCategory ' + categoryName });
+});
+
 exports.deletecategory = catchAsync(async (req, res, next) => {
   const { categoryId } = req.body;
   const deletecategory =
     'UPDATE azst_category_tbl SET azst_category_status = 0, azst_updated_by=? where azst_category_id = ? ';
   const values = [req.empId, categoryId];
+
+  await db(deletecategory, values);
+  res.status(200).json({ message: 'category deleted Successfully ' });
+});
+
+exports.deleteSubCategory = catchAsync(async (req, res, next) => {
+  const { subCategoryId } = req.body;
+  const deletecategory =
+    'UPDATE azst_sub_category_tbl SET azst_sub_category_status = 0, updated_by=? where azst_sub_category_id = ? ';
+  const values = [req.empId, subCategoryId];
 
   await db(deletecategory, values);
   res.status(200).json({ message: 'category deleted Successfully ' });
--- routes/AdminRoutes/categoryRoutes.js
+++ routes/AdminRoutes/categoryRoutes.js
@@ -1,15 +1,24 @@
 const router = require('express').Router();
-
+const multer = require('multer');
 const categoryCtrl = require('../../controllers/AdminCtrls/categoryCtrl');
 const authCtrl = require('../../controllers/authController');
 
+router.use(multer().any());
+
 router.get('/data', categoryCtrl.getcategories);
+router.get('/sub-categories', categoryCtrl.getSubcategories);
 
 const key = process.env.JWT_SECRET_ADMIN;
 
 router.use(authCtrl.protect(key));
+
 router.post('/add', categoryCtrl.addcategory);
 router.post('/add-sub', categoryCtrl.addSubCategory);
+router.get('/data/categories', categoryCtrl.getAdminSubCategories);
+
+router.post('/edit', categoryCtrl.updateSubCategory);
+router.post('/delete-sub', categoryCtrl.deleteSubCategory);
+router.post('/get-sub', categoryCtrl.getSubCategory);
 
 router.use(categoryCtrl.isCategoryExit);
 router
