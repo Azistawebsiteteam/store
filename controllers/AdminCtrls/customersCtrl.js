1a43be8530c21e39e2cee3152febb8aa570e222b
Rajender <rajender.cheerneni@heterohealthcare.com>
2024-06-20 12:45:38 +0530

    sort and filters added in user api

--- controllers/AdminCtrls/customersCtrl.js
+++ controllers/AdminCtrls/customersCtrl.js
@@ -3,36 +3,58 @@ const db = require('../../dbconfig');
 const catchAsync = require('../../Utils/catchAsync');
 
 exports.getAllCustomers = catchAsync(async (req, res, next) => {
-  const { isActive, orderby } = req.body;
-  const { orderbyKey, sort } = orderby;
+  let { isActive, orderbyKey, sort } = req.body;
+
+  orderbyKey = orderbyKey ? orderbyKey : 'azst_customer_createdon';
+
+  sort = sort ? sort : 'DESC';
+
+  // Validating the orderbyKey and sort parameters
+  const validOrderbyKeys = {
+    totalorder: 'azst_customer_totalorders',
+    totalamountspent: 'azst_customer_totalspent',
+    lastupdated: 'azst_customer_updatedon',
+    default: 'azst_customer_createdon ',
+  };
+
+  const validSortOrders = ['ASC', 'DESC'];
+
+  // Construct the orderbyQuery using validated inputs
+  const orderbyQuery =
+    validOrderbyKeys[orderbyKey.toLowerCase()] || validOrderbyKeys.default;
+
+  const sortOrder = validSortOrders.includes(sort.toUpperCase())
+    ? sort.toUpperCase()
+    : 'DESC';
+
+  // Construct the filterQuery securely using parameterized queries
   let filterQuery = '';
-  if (isActive) {
-    filterQuery = `WHERE azst_customer_status = ${isActive}`;
+  let queryParams = [];
+  if (typeof isActive !== 'undefined') {
+    filterQuery = 'WHERE azst_customer_status = ?';
+    queryParams.push(isActive);
   }
 
-  // Constructing order by query based on the orderby field
-  let orderbyQuery = '';
-  switch (orderbyKey ? orderbyKey.toLowerCase() : '') {
-    case 'totalorder':
-      orderbyQuery = `azst_customer_totalorders ${sort}`;
-      break;
-    case 'totalamountspent':
-      orderbyQuery = `azst_customer_totalspent ${sort} `;
-      break;
-    case 'lastupdated':
-      orderbyQuery = `azst_customer_updatedon ${sort}`;
-      break;
-    default:
-      orderbyQuery = `azst_customer_createdon ${sort}`;
-  }
+  const getUsers = `
+    SELECT 
+      azst_customer_id,
+      azst_customer_fname,
+      azst_customer_lname,
+      azst_customer_mobile,
+      azst_customer_email,
+      azst_customer_acceptemail_marketing,
+      azst_customer_status,
+      DATE_FORMAT(azst_customer_createdon, '%d-%m-%Y %H:%i:%s') as registered_on,
+      azst_customer_gender,
+      azst_customer_state,
+      azst_customer_country,
+      azst_customer_totalspent,
+      azst_customer_totalorders
+    FROM azst_customer
+    ${filterQuery}
+    ORDER BY ${orderbyQuery} ${sortOrder}`;
 
-  const getUsers = ` SELECT azst_customer_id,azst_customer_fname,azst_customer_lname,
-                            azst_customer_mobile,azst_customer_email,azst_customer_acceptemail_marketing,
-                            azst_customer_status,DATE_FORMAT(azst_customer_createdon , '%d-%m-%Y %H:%i:%s') as registered_on,
-                            azst_customer_gender   , azst_customer_state,  azst_customer_country, azst_customer_totalspent,
-                            azst_customer_totalorders
-                        FROM azst_customer  ${filterQuery}  ORDER BY  ${orderbyQuery}`;
-  const users = await db(getUsers);
+  const users = await db(getUsers, queryParams);
   res.status(200).json(users);
 });
 
