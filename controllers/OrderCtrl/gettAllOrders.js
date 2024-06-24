ccc831a19c90c85cb5bc1de94f45e3d677717e80
Rajender <rajender.cheerneni@heterohealthcare.com>
2024-06-24 17:29:33 +0530

    order module updated customer orders api

--- controllers/OrderCtrl/gettAllOrders.js
+++ controllers/OrderCtrl/gettAllOrders.js
@@ -26,8 +26,8 @@ exports.getAllOrdrs = catchAsync(async (req, res, next) => {
                             IFNULL(SUM(azst_ordersummary_tbl.azst_order_qty), 0) AS items , 
                            CONCAT(azst_customer_fname , ' ' , azst_customer_lname) AS customer_name
                        FROM azst_orders_tbl
-                       LEFT JOIN azst_ordersummary_tbl
-                       ON azst_orders_tbl.azst_orders_id = azst_ordersummary_tbl.azst_orders_id
+                        LEFT JOIN azst_ordersummary_tbl
+                        ON azst_orders_tbl.azst_orders_id = azst_ordersummary_tbl.azst_orders_id
                         LEFT JOIN azst_customer
                         ON azst_customer.azst_customer_id = azst_orders_tbl.azst_orders_customer_id 
                        ${filterQuery}
@@ -43,7 +43,7 @@ exports.getCustomerOrders = catchAsync(async (req, res, next) => {
 
 exports.getOrderDetails = catchAsync(async (req, res, next) => {
   const { orderId } = req.body;
-
+  console.log(orderId);
   const schema = Joi.object({
     orderId: Joi.string().min(1).required(),
   });
@@ -60,8 +60,7 @@ exports.getOrderDetails = catchAsync(async (req, res, next) => {
         'azst_order_product_id', azst_ordersummary_tbl.azst_order_product_id,
         'azst_order_variant_id', azst_ordersummary_tbl.azst_order_variant_id,
         'image_src', azst_products.image_src,
-        'price', azst_products.price,
-        'offer_price', azst_sku_variant_info.offer_price,
+        'azst_product_price' ,azst_ordersummary_tbl.azst_product_price,
         'option1', azst_sku_variant_info.option1,
         'option2', azst_sku_variant_info.option2,
         'option3', azst_sku_variant_info.option3,
@@ -96,52 +95,3 @@ exports.getOrderDetails = catchAsync(async (req, res, next) => {
   };
   res.status(200).json(Order);
 });
-
-// azst_orders_tbl.azst_orders_id,
-//       azst_orders_tbl.azst_orders_email,
-//       azst_orders_tbl.azst_orders_financial_status,
-//       azst_orders_tbl.azst_orders_paid_on,
-//       azst_orders_tbl.azst_orders_fulfillment_status,
-//       azst_orders_tbl.azst_orders_fulfilled_on,
-//       azst_orders_tbl.azst_orders_currency,
-//       azst_orders_tbl.azst_orders_subtotal,
-//       azst_orders_tbl.azst_orders_shipping,
-//       azst_orders_tbl.azst_orders_taxes,
-//       azst_orders_tbl.azst_orders_total,
-//       azst_orders_tbl.azst_orders_discount_code,
-//       azst_orders_tbl.azst_orders_discount_amount,
-//       azst_orders_tbl.azst_orders_shipping_method,
-//       azst_orders_tbl.azst_orders_status,
-//       azst_orders_tbl.azst_orders_created_on,
-//       azst_orders_tbl.azst_orders_customer_id,
-//       azst_orders_tbl.azst_orders_checkout_id,
-//       azst_orders_tbl.azst_orders_cancelled_at,
-//       azst_orders_tbl.azst_orders_payment_method,
-//       azst_orders_tbl.azst_orders_payment_reference,
-//       azst_orders_tbl.azst_orders_vendor,
-//       azst_orders_tbl.azst_orders_vendor_code,
-//       azst_orders_tbl.azst_orders_tags,
-//       azst_orders_tbl.azst_orders_source,
-//       azst_orders_tbl.azst_orders_billing_province_name,
-//       azst_orders_tbl.azst_orders_shipping_province_name,
-//       azst_orders_tbl.azst_orders_payment_id,
-//       azst_orders_tbl.azst_orders_payment_references,
-
-//   CONCAT(
-//     '[',
-//     GROUP_CONCAT(
-//       JSON_OBJECT(
-// 'product_title', azst_products.product_title,
-// 'azst_order_product_id', azst_ordersummary_tbl.azst_order_product_id,
-// 'azst_order_variant_id', azst_ordersummary_tbl.azst_order_variant_id,
-// 'image_src', azst_products.image_src,
-// 'price', azst_products.price,
-// 'offer_price', azst_sku_variant_info.offer_price,
-// 'option1', azst_sku_variant_info.option1,
-// 'option2', azst_sku_variant_info.option2,
-// 'option3', azst_sku_variant_info.option3,
-// 'azst_order_qty', azst_ordersummary_tbl.azst_order_qty
-//       )
-//     ),
-//     ']'
-//   ) AS products_details
